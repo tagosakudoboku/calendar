8ddfa1a485c7f35e89c2c1b8e13b532f2b576eac
tagosaku <tagosaku.doboku@gmail.com>
2024-12-09 21:33:02 +0900

    update

---
+++ src/components/function.js
@@ -0,0 +1,17 @@
+export function isToday(date)
+{
+    const local_date = new Date();
+    return local_date === date;
+}
+
+export function calcDateID(date)
+{
+
+}
+
+export function convert2ActivityObj(param)
+{
+    return {
+
+    };
+}
